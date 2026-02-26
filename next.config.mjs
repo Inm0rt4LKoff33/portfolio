9aeacbcf8fcaa2b98b8756de133a58b738b945e8
Inm0rt4LKoff33 <fabimv23@gmail.com>
2026-02-26 16:41:57 -0600

    paths added to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
   output: "export",   // enables static export for GitHub Pages
   trailingSlash: true,
   images: { unoptimized: true },
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio",
 };
 
 export default nextConfig;
